89f14ea531cef660289119dbc47afd0d798a720e
Richard Jones <richard@cottagelabs.com>
2015-07-31 08:40:54 +0100

    minor tweaks to include preferred deployment of storage module

--- config/service.py
+++ config/service.py
@@ -41,5 +41,5 @@ SECRET_KEY = "super-secret-key"
 #STORE_TMP_IMPL = "octopus.modules.store.store.TempStore"
 
 from octopus.lib import paths
-STORE_LOCAL_DIR = paths.rel2abs(__file__, "..", "service", "tests", "local_store")
-STORE_TMP_DIR = paths.rel2abs(__file__, "..", "service", "tests", "local_store")
+STORE_LOCAL_DIR = paths.rel2abs(__file__, "..", "service", "tests", "local_store", "live")
+STORE_TMP_DIR = paths.rel2abs(__file__, "..", "service", "tests", "local_store", "tmp")
---
+++ service/tests/local_store/tmp/README.md
@@ -0,0 +1,3 @@
+This directory is used by octopus.modules.store.store.TempStore as a basic local teporary storage system.
+
+You shouldn't check any sub-directories from here into the repo
