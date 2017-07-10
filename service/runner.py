cf4b2a49c6c3665bbacf06450a598611b41e3909
Steven Eardley <steve@eardley.xyz>
2017-07-10 11:00:55 +0100

    Added timestamp to flask debug log

--- service/runner.py
+++ service/runner.py
@@ -1,8 +1,21 @@
 from service import workflow
 from octopus.core import app, initialise
+import flask.logging
 
 if __name__ == "__main__":
     initialise()
+
+    if app.debug:
+        # Augment the default flask debug log to include a timestamp
+        app.debug_log_format = (
+            '-' * 80 + '\n' +
+            '%(asctime)s\n'
+            '%(levelname)s in %(module)s [%(pathname)s:%(lineno)d]:\n' +
+            '%(message)s\n' +
+            '-' * 80
+        )
+        flask.logging.create_logger(app)
+
     accs = app.config.get("API_KEYS", {}).keys()
     for account_id in accs:
         workflow.HarvesterWorkflow.process_account(account_id)
